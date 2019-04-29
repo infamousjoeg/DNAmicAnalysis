a3178b6f2adc8acdebbb98400904f9e4e05605c6
Joe Garcia <joe@joe-garcia.com>
2019-04-29 16:13:33 -0400

    Fixed argparse and logzero; updated README

--- DNAmicAnalysis.py
+++ DNAmicAnalysis.py
@@ -8,35 +8,22 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 import argparse
+import logging
 import sqlite3
 from sqlite3 import Error
 
 import logzero
 from logzero import logger
 
+LOGFILE = 'DNAmicAnalysis.log'
 
-def config_logger(logfile, debug):
+def config_logger(logfile):
     """ Configures logging of this application """
-    # Format logfile path in logfile variable
-    if logfile is None:
-        logfile = "DNAmicAnalysis.log"
-    else:
-        logfile = logfile.rstrip()
-        if logfile.endswith('/'):
-            logfile = logfile[:-1]
-        logfile = logfile + "/DNAmicAnalysis.log"
-
     # Set a minimum log level
-    if debug is None:
-        logzero.loglevel(logging.INFO)
-    else:
-        logzero.loglevel(logging.DEBUG)
+    logzero.loglevel(logging.INFO)
 
     # Set a logfile (all future log messages are also saved there), but disable the default stderr logging
     logzero.logfile(logfile, disableStderrLogger=True)
-
-    # Log to syslog, using default logzero logger and 'local0' syslog facility
-    logzero.syslog(facility=SysLogHandler.LOG_LOCAL0)
 
     # Set a custom formatter
     formatter = logging.Formatter('%(name)s - %(asctime)-15s - %(levelname)s: %(message)s');
@@ -93,7 +80,7 @@ def main(args):
     logger.debug("Arguments received: ", args)
 
     # Create database connection
-    conn = create_connection(database)
+    conn = create_connection(database_file)
     with conn:
         expired_domain = exec_fromfile(data/sql/ExpiredDomainPrivID.sql)
         expired_local = exec_fromfile(data/sql/UniqueExpiredLocalPrivID.sql)
@@ -102,7 +89,7 @@ def main(args):
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     # Configure logging
-    config_logger(logfile, debug)
+    config_logger(LOGFILE)
     logger.info("Application initialized successfully")
 
     # Create argument parsing object
@@ -111,7 +98,6 @@ if __name__ == "__main__":
     # Required positional argument for database file to query against
     parser.add_argument(
         "database_file",
-        dest="dbfile",
         help="Path to the CyberArk DNA SQLite3 database file")
 
     # Optional argument flag to set minimum log level to DEBUG - Default: INFO
@@ -121,14 +107,6 @@ if __name__ == "__main__":
         dest="debug",
         default=False,
         help="Sets DEBUG as the minimum log level [Default: INFO]")
-
-    # Optional argument flag to set the log file path - Default: root directory of script
-    parser.add_argument(
-        "-l",
-        "--logfile",
-        dest="logfile",
-        help="Sets the path for the log file to be generated [Default: root directory of script]"
-    )
 
     # Optional argument flag to output current version
     parser.add_argument(
--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 
 Automation for CyberArk's Discovery & Audit (DNA) reports.
 
+## Usage
+
+```shell
+$ ./DNAmicAnalysis.py -h
+usage: DNAmicAnalysis.py [-h] [--debug] [-v] database_file
+
+CyberArk DNA report generation utility
+
+positional arguments:
+  database_file  Path to the CyberArk DNA SQLite3 database file
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --debug        Sets DEBUG as the minimum log level [Default: INFO]
+  -v, --version  Displays current version information
+```
+
+## Version
+
+```shell
+$ ./DNAmicAnalysis.py -v
+DNAmicAnalysis.py (version 0.1.0)
+```
+
 ## License
 
 MIT
