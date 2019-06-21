7c3c93d573e015e139074f9c2fe3934e8b677eea
Joe Garcia <joe@joe-garcia.com>
2019-06-21 16:30:28 -0400

    Added back var

--- data/sql/UniqueExpiredDomainPrivID.sql
+++ data/sql/UniqueExpiredDomainPrivID.sql
@@ -1,6 +1,6 @@
 SELECT Accounts.Name, Machines.Address, OSAccounts.LastPasswordSet,
-	MAX(Cast ((JulianDay(datetime('2019-05-21 20:57:43')) - JulianDay(OSAccounts.LastPasswordSet)) As Integer)) as MaxPasswordAge,
-	AVG(Cast ((JulianDay(datetime('2019-05-21 20:57:43')) - JulianDay(OSAccounts.LastPasswordSet)) As Integer)) as AvgPasswordAge
+	MAX(Cast ((JulianDay(datetime('{scanDateTime}')) - JulianDay(OSAccounts.LastPasswordSet)) As Integer)) as MaxPasswordAge,
+	AVG(Cast ((JulianDay(datetime('{scanDateTime}')) - JulianDay(OSAccounts.LastPasswordSet)) As Integer)) as AvgPasswordAge
 FROM Accounts
 	LEFT OUTER JOIN OSAccounts
 		ON Accounts.Id = OSAccounts.AccountBase_id
@@ -8,7 +8,7 @@ FROM Accounts
 		ON Accounts.Id = OSGroupModel.OSAccount_id
 	LEFT OUTER JOIN Machines
 		ON Accounts.Machine_id = Machines.Id
-WHERE OSAccounts.LastPasswordSet <= datetime('2019-05-21 20:57:43', '-90 days')
+WHERE OSAccounts.LastPasswordSet <= datetime('{scanDateTime}', '-90 days')
 	AND Accounts.AccountType = 'Domain'
 	AND OSGroupModel.DomainGroup LIKE '%Domain Admins%'
 GROUP BY Accounts.Name
