8b0b38232dbd9fb2c95cf886cef489298f05e4d5
Joe Garcia <joe@joe-garcia.com>
2019-07-22 16:58:19 -0400

    Added filter for Administrator usernames

--- data/sql/LocalAbandonedAccounts.sql
+++ data/sql/LocalAbandonedAccounts.sql
@@ -10,6 +10,7 @@ FROM Accounts
 WHERE OSAccounts.LastPasswordSet <= datetime('{scanDateTime}', '-90 days')
 	AND OSAccounts.LastLogon <= datetime('{scanDateTime}', '-1 year')
 	AND Accounts.AccountType = 'Local'
+	AND NOT Accounts.Name LIKE 'Administrator'
 	AND (OSGroupModel.Name LIKE 'Administrators'
 		OR OSGroupModel.Name LIKE 'Power Users')
 	{disabled}
