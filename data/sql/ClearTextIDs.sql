df4e157d12921cb36f45c30492a55d2d4ed0296d
Joe Garcia <joe@joe-garcia.com>
2019-10-09 17:07:34 -0400

    Removed duplicate WHERE filter

--- data/sql/ClearTextIDs.sql
+++ data/sql/ClearTextIDs.sql
@@ -1,7 +1,6 @@
 SELECT Name, COUNT(Name) as TotalFound, PasswordLength
 FROM HardCodedPasswordAccounts
 WHERE NOT (Name LIKE '%*%'
-		OR Name LIKE ''
 		OR Name LIKE 'S-%'
 		OR Name LIKE '')
 GROUP BY LOWER(Name)
