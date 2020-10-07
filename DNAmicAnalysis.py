eb6c02bf4d7f491bf52dbe0a23cc146c6b2c708f
Joe Garcia <joe@joe-garcia.com>
2020-10-07 14:15:16 -0400

    prepare for patch v2.1.1

--- DNAmicAnalysis.py
+++ DNAmicAnalysis.py
@@ -18,7 +18,7 @@ from logzero import logger
 from dnamic_analysis import Database, DomainCheck, Xlsx, Metrics, Output
 
 __author__ = "Joe Garcia"
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 __license__ = "MIT"
 
 log_timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
