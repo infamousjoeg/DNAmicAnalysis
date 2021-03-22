9f869c4a9047117963940a4a30b9d96db0014d5a
Joe Garcia <joe@joe-garcia.com>
2021-03-22 14:46:00 -0400

    Add SQL Queries for Unix Scan Data Metrics

---
+++ data/sql/Unix_DomainAbandonedAccounts.sql
@@ -0,0 +1,13 @@
+SELECT Accounts.Name, Machines.Address
+FROM Accounts
+	LEFT OUTER JOIN OSAccounts
+		ON Accounts.Id = OSAccounts.AccountBase_id
+	LEFT OUTER JOIN Machines
+		ON Accounts.Machine_id = Machines.Id
+WHERE OSAccounts.LastPasswordSet <= datetime('{scanDateTime}', '-{executionDays} days')
+	AND OSAccounts.LastLogon <= datetime('{scanDateTime}', '-1 year')
+	AND Accounts.AccountType != 'Local'
+	AND NOT Accounts.Name = 'root'
+	AND Machines.Platform = 'Nix'
+	AND OSAccounts.Enabled = {disabled}
+GROUP BY LOWER(Accounts.Name)
---
+++ data/sql/Unix_ExpiredRootPasswords.sql
@@ -0,0 +1,15 @@
+SELECT Machines.Address,
+	MAX(Cast ((JulianDay(datetime('{scanDateTime}')) - JulianDay(OSAccounts.LastPasswordSet)) As Integer)) as MaxPasswordAge,
+	COUNT(DISTINCT Machines.Address) as NumMachines
+FROM Accounts
+	LEFT OUTER JOIN OSAccounts
+		ON Accounts.Id = OSAccounts.AccountBase_id
+	LEFT OUTER JOIN Machines
+		ON Accounts.Machine_id = Machines.Id
+WHERE Machines.Platform = 'Nix'
+	AND OSAccounts.LastPasswordSet <= datetime('{scanDateTime}', '-{executionDays} days')
+	AND OSAccounts.LastPasswordSet != ''
+	AND Accounts.Name = 'root'
+	AND OSAccounts.Enabled = {disabled}
+GROUP BY LOWER(Machines.Address)
+ORDER BY MaxPasswordAge DESC
